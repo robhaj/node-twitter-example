184e56b3a0eedfbe3d9ae2d6a99cf44edc4b5c20
Robert Hajek <hajek.rob@gmail.com>
2016-03-29 22:19:52 -0600

    added dummy config file

---
+++ _config_example.js
@@ -0,0 +1,9 @@
+var config = {};
+
+// twitter keys
+config.consumer_key = 'changeme';
+config.consumer_secret = 'changeme';
+config.access_token_key = 'changeme';
+config.access_token_secret = 'changeme';
+
+module.exports = config;
